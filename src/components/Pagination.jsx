27f141e8df5bf5cded0e66083841b83f1b3e3fb7
Kassim-Omomkolade <omokoladekassim@gnail.com>
2024-12-10 15:58:56 +0100

    added function

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Pagination = () => {
+const Pagination = ({ currentPage, setCurrentPage, numOfPages }) => {
   const buttonStyles = (isDisabled) => ({
     cursor: isDisabled ? "not-allowed" : "pointer",
   });
@@ -8,14 +8,19 @@ const Pagination = () => {
     <div className="container d-flex justify-content-between align-items-center py-5 pagination-container">
       <button
         className="border border-dark border-2 text-dark rounded-1 fs-5 fw-medium"
-        style={buttonStyles(true)}
+        style={buttonStyles(currentPage === 1)}
+        onClick={() => setCurrentPage(currentPage - 1)}
       >
         Previous
       </button>
-      <p>Page 1 of 10</p>
+      <p>
+        Page {currentPage} of {numOfPages}
+      </p>
       <button
         className="main-bg-color text-white rounded-1 border-0 fs-5 fw-medium specialbtn"
-        style={buttonStyles(false)}
+        style={buttonStyles(currentPage === numOfPages)}
+        disabled={currentPage === numOfPages}
+        onClick={() => setCurrentPage(currentPage + 1)}
       >
         Next
       </button>
--- src/pages/EventDetails.jsx
+++ src/pages/EventDetails.jsx
@@ -3,9 +3,43 @@ import Layout from "../components/Layout";
 import OthersLiked from "../components/singleEvent Components/OthersLiked";
 import { events } from "../data/data";
 import EventProperties from "../components/singleEvent Components/EventProperties";
+import axios, { Axios } from "axios";
+import { useParams } from "react-router-dom";
+import Loader from "../components/Loader";
+import { useState, useEffect } from "react";
 
 const EventDetails = () => {
   const { ...all } = events[1];
+  const url = "https://nb-event-server.onrender.com/api/v1/events";
+  const { eventId } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [event, setEvent] = useState(null);
+  const [similarEvents, setSimilarEvents] = useState([]);
+
+  const getEvent = async () => {
+    try {
+      const { data } = await axios(`${url}/${eventId}`);
+      console.log(data);
+      setIsLoading(false);
+      setEvent(data.event);
+      setSimilarEvents(data.similarEvents);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getEvent();
+  }, []);
+  if (isLoading) {
+    return (
+      <>
+        <Layout>
+          <Loader height="200px" />
+        </Layout>
+      </>
+    );
+  }
+
   return (
     <>
       <Layout>
@@ -15,8 +49,8 @@ const EventDetails = () => {
             <span className="main-color">Event Details</span>
           </h3>
         </div>
-        <EventProperties {...all} />
-        <OthersLiked />
+        <EventProperties {...event} />
+        {similarEvents.length > 0 && <OthersLiked />}
       </Layout>
     </>
   );
